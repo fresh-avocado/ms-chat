3ad17c786c21307b9f20db2affc02dde9cf0df3a
Gabriel Spranger <gabriel.spranger@utec.edu.pe>
2023-08-09 20:53:04 +0000

    feat: dev dockerfile & .env reader

--- src/app.controller.spec.ts
+++ src/app.controller.spec.ts
@@ -1,12 +1,16 @@
 import { Test, TestingModule } from '@nestjs/testing';
 import { AppController } from './app.controller';
 import { AppService } from './app.service';
+import { ConfigModule } from '@nestjs/config';
 
 describe('AppController', () => {
   let appController: AppController;
 
   beforeEach(async () => {
     const app: TestingModule = await Test.createTestingModule({
+      imports: [ConfigModule.forRoot({
+        envFilePath: '.env.dev',
+      })],
       controllers: [AppController],
       providers: [AppService],
     }).compile();
